ece5cf7182f8f6516b4eb28c9db08516f3b3d550
John Mooney <jrmint46@gmail.com>
2017-06-06 00:46:13 -0400

    Completed the third exercise

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,46 @@
-#write your code here
+def add(x, y)
+	x + y
+end
+
+def subtract(x, y)
+	x - y
+end
+
+def sum(array)
+	x = 0
+	array.each do |y|
+		x = x + y
+	end
+	x
+end
+
+def multiply(array)
+	x = 1
+	array.each do |y|
+		x = x * y
+	end
+	x
+end
+
+def power(x, y)
+	z = 1
+	y.times do
+		z = z * x
+	end
+	z
+end
+
+def factorial(x)
+	y = 1
+	if x == 0
+		y = 1
+	end
+	while x > 0
+		y = y * x
+		x = x - 1
+	end
+	y
+end
+
+
+
