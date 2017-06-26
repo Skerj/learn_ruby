af866ea7b6f7e9cd3aea84cfd9f6a6901207fdac
John Mooney <jrmint46@gmail.com>
2017-06-26 09:50:05 -0400

    Finished book titles exercise

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,19 @@
 class Book
-# write your code here
+  attr_accessor :title
+  def title
+    dont_capitalize = "a an the and but or nor at by for from in into of off on onto out up with"
+    complete = []
+    original = @title.downcase.split
+    original.each do |word|
+      if word == "i"
+        complete.push(word.upcase)
+      elsif dont_capitalize.include? word
+        complete.push(word)
+      else
+        complete.push(word.capitalize)
+      end
+      complete[0].capitalize!
+    end
+  complete.join(" ")
+  end  
 end
